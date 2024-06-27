1bf58e9ecb80811ac61112a163b17b980cdffc59
DolapoSoares <soaresdolapo@gmail.com>
2024-06-27 11:26:03 +0100

    creating the project

---
+++ voyatek/src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Settings
+() {
+  return (
+  <div>
+   <div class="relative">
+    <section>
+      <img src="https://res.cloudinary.com/soares04/image/upload/v1719481755/tkr1ptr7hl0htcbwdmd4.png" alt="go logo"/>
+    </section>
+    <section className="flex items-center bg-gray-200 rounded-lg p-2 w-full max-w-sm mx-auto">
+      <i className="fas fa-search text-gray-500 mr-2"></i>
+      <input
+        type="search"
+        placeholder="Search here"
+        className="bg-gray-900 rounded-md py-1 px-2 outline-none w-full text-gray-700"
+      />
+    </section>
+   </div>
+   <div>
+    
+   </div>
+  </div>
+  )
+}
+
+export default Settings
